4b33dec624e6c7a88e56bcc03875a092faf147d0
Shruti <sharma224@wisc.edu>
2022-07-30 23:42:39 -0500

    comments

--- server/controllers/pointsController.js
+++ server/controllers/pointsController.js
@@ -1,16 +1,18 @@
 /** @format */
 const { json } = require("body-parser");
-const mongoose = require("mongoose");
-const { update } = require("../models/addPoints.js");
+require("mongoose");
+require("../models/addPoints.js");
 require("../models/db");
 
 const addPoints = require("../models/addPoints.js");
 
 /**
+ * Deducts given points and Returns an array of objects containing information about how
+ * much was deducted from reward points from which payers.
  *
- * @param {*} pointsToSpend The amount to spend
- * @param {*} results Array of current transactions sorted by timestamp (ascending)
- * @return
+ * @param {Number} pointsToSpend The amount of points to spend from user's account.
+ * @param {Array} results Array of current transactions sorted by timestamp (ascending).
+ * @return {Array} An array of Json Objects {"payer" :,  "points": } displaying subtracted amount from payers.
  */
 async function spend(pointsToSpend, results) {
   var afterSpending = {};
@@ -18,8 +20,6 @@ async function spend(pointsToSpend, results) {
 
   while (pointsToSpend > 0) {
     let id = results[i]._id;
-    //if payer already exists as key in dictionary afterSpending, subtract from existing amount
-    //else substract from 0
     var pointsAvailable = results[i].points;
     if (results[i].points >= pointsToSpend) {
       afterSpending[results[i].payer] = -1 * pointsToSpend;
@@ -37,7 +37,7 @@ async function spend(pointsToSpend, results) {
     pointsToSpend = pointsToSpend - pointsAvailable;
     i++;
   }
-  //Create doc from each key-value pair in afterSpending dictionary and save in spendPoints Schema
+
   let arrOfJsonObjs = [];
   for (const [key, value] of Object.entries(afterSpending)) {
     arrOfJsonObjs.push({ payer: key, points: value });
@@ -45,33 +45,43 @@ async function spend(pointsToSpend, results) {
   return arrOfJsonObjs;
 }
 
+/**
+ * Updates points field of documents with given id
+ *
+ * @param {Number} points New points after deductions
+ * @param {_id} id id of the document
+ */
 async function updateDoc(points, id) {
   const doc = await addPoints.findById(id);
   doc.points = points;
-  console.log("updated points = ", doc.points);
-  console.log("doc = ", doc);
   await doc.save();
 }
 
-//GET request for spending request
 /**
- * @return array of json objects indicating spent points from their respective payers
+ * GET request to spend requested number of points
+ *
+ * @param {*} req
+ * @param {*} res
+ * @return {JSON} sends response of json objects received from spend() function
  */
 exports.spendPoints = async (req, res) => {
   try {
     const pointsToSpend = req.body.points;
     const results = await addPoints.find({}).sort("timestamp");
-    console.log("Results : ", results);
     let arrOfJsonObjs = await spend(pointsToSpend, results);
-    console.log("Results after spending: ", results);
-    console.log("arr of json objs: ", arrOfJsonObjs);
     res.json(arrOfJsonObjs);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
 
-//POST request to add points
+/**
+ * POST request to add points (i.e create docs for each transaction in database)
+ *
+ * @param {*} req
+ * @param {*} res
+ * @return {JSON} sends a JSON object for each doc converted to json format as respnse
+ */
 exports.addPoints = async (req, res) => {
   try {
     var isJson = await isJsonObject(req.body);
@@ -102,24 +112,29 @@ exports.addPoints = async (req, res) => {
 };
 
 /**
+ * Function to GET request body is an array of JSON objects or just 1 object (i.e one transaction)
  *
- * @param {array|json} strData  data passed in in the body of addPoints GET request
- * @returns
+ * @param {any} data data passed in in the body of addPoints GET request
+ * @returns {bool} true if data is JSON, else fasle
  */
-async function isJsonObject(strData) {
+async function isJsonObject(data) {
   try {
-    JSON.parse(strData);
+    JSON.parse(data);
   } catch (e) {
     return false;
   }
   return true;
 }
 
-//GET request to balance points
+/**
+ * GET request to balance points after spending points
+ *
+ * @param {*} req
+ * @param {*} res
+ */
 exports.balancePoints = async (req, res) => {
   try {
     var grouped = await balance();
-    console.log("Grouped returned = ", grouped);
     res.json(grouped);
   } catch (error) {
     res.status(400).json({ message: error });
@@ -127,6 +142,12 @@ exports.balancePoints = async (req, res) => {
   }
 };
 
+/**
+ * Groups all documents based on field "payer" and sums "points".
+ *
+ * @returns {Object} a dictionary of key-value pairs of payer (key) and remaining points (value)
+ *                  based on the aggregation.
+ */
 async function balance() {
   let balancedOutput = {};
   const grouped = await addPoints.aggregate([
